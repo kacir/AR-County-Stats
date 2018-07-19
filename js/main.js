151820db0bc40f94af5f994b0433f5a665c0a35f
Robert Kacir <kacir@wisc.edu>
2018-07-18 21:22:26 -0500

    Load Data code
    added code to load the needed data. Also created most of the loop needed to join the spatial and non-spatial data together.

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ function runScript () {
     var xpadding = 20;
     var ypadding = 25;
     
+    var dataList = [];
+    
+    
     //set the inital characertistics of the map svg
     var map = d3.select("#map")
         .attr("height" , mapheight)
@@ -22,7 +25,45 @@ function runScript () {
     
     
     //ajax function that starts to load data
-    
+    Promise.all([
+        d3.csv("data/counties-nonspatial.csv"),
+        d3.json("data/Counties.json")
+    ]).then(function (files) {
+        console.log("This appears to work!");
+        
+        var nonSpatialData = files[0];
+        var spatialData = files[1].features;
+        
+        console.log("logging non-spatial data");
+        console.log(nonSpatialData);
+        console.log("logging spatial data");
+        console.log(spatialData);
+        
+        //join the spatial and non spatial data together
+        for (var i=0; i < nonSpatialData.length; i++) {
+            var nonSpatialCounty = nonSpatialData[i];
+            var nonSpatialKey = nonSpatialCounty.GISCountyName;
+            
+          for (var a=0; a < spatialData.length; a++){
+              
+              var spatialCounty = spatialData[a];
+              var spatialKey = spatialCounty.properties.name;
+              
+              if (spatialKey == nonSpatialKey){
+                  console.log("The Keys Match!");
+                  
+                  //join the two datasets together and push the object into the new master array
+                  
+              }
+              
+          }
+        }
+        
+    }).catch( function (error) {
+        console.log("Something has failed" + error);
+        
+    });
+        
     
     
     console.log("testing");
