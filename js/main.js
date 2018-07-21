cd5cdb7270ab55112cbcd9890456a8f8f4c5faff
Robert Kacir <kacir@wisc.edu>
2018-07-21 09:56:51 -0500

    elements added
    managed to get the data driven elements to be added into the dom. still need to style them.

--- js/main.js
+++ js/main.js
@@ -10,17 +10,18 @@ function runScript () {
     var xpadding = 20;
     var ypadding = 25;
     
-    var dataList = [];
+    var dataList = [];//array that will hold all of the master joined data, d3 will have to work with this array
+    //array with the field name of interest in the master non-spatial table
     var attrArray = ["Bachelors" , "GISCountyName" , "LessthanHighschool" , "Miles of Interstate", "OnlyHighschool", "PovertyPer", "medianIncome", "someCollege", "unempolymentPer"];
     
-    /*
-    var projection = d3.geo.albers()
+    var projection = d3.geoConicEqualArea()
         .center([3.64, 33.60])
         .rotate([95.55, 0, 0])
         .parallels([22.91, 55.02])
         .scale(2573.74)
         .translate(mapwidth / 2, mapheight / 2);
-    */
+    
+    var pathGenerator = d3.geoPath().projection(projection);
     
     
     //set the inital characertistics of the map svg
@@ -33,12 +34,18 @@ function runScript () {
         .attr("height" , graphheight)
         .attr("width" , graphwidth);
     
+    //set scales that will be used inside of the graph to scale the bars correctly
+    var ycord = "";
+    var xcord = '';
+    var color = '';
+    var height = '';
     
-    //ajax function that starts to load data
-    Promise.all([
-        d3.csv("data/counties-nonspatial.csv"),
-        d3.json("data/Counties.json")
-    ]).then(function (files) {
+    
+    
+    
+    //merges the spatial and non-spatial data together into a single list of joined objects.
+    //function is called after ajax request has been filled.
+    function joinData (files) {
         console.log("This appears to work!");
         
         var nonSpatialData = files[0];
@@ -80,6 +87,48 @@ function runScript () {
         
         console.log("Logging joined data!");
         console.log(dataList);
+    }
+    
+    function createMap () {
+        console.log("create map called");
+        
+        var counties = map.selectAll(".counties")
+            .append("path")
+            .data(dataList)
+            .enter()
+            .append("path")
+            .attr("class" , ".counties");
+        
+    }
+    
+    function createGraph () {
+        console.log("create graph called");
+        
+        var fistAttr = attrArray[0];
+        
+        var bars = graph.selectAll(".bars")
+            .data(dataList)
+            .enter()
+            .append("rect")
+            .attr("class" , ".bars")
+            .attr("style", "fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)")
+            .attr("width" , "50")
+            .attr("height" , "100")
+            ;
+        
+    }
+    
+    
+    
+    //ajax function that starts to load data
+    Promise.all([
+        d3.csv("data/counties-nonspatial.csv"),
+        d3.json("data/Counties.json")
+    ]).then(function (files) {
+        joinData(files);//merge data from the two different files together
+        createMap();
+        createGraph();
+
         
     }).catch( function (error) {
         console.log("Something has failed" + error);
